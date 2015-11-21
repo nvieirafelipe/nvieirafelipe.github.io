e29b6bd56bd4857560025510033d56ca75a3434a
Felipe Vieira <nvieirafelipe@gmail.com>
2015-11-20 23:10:22 -0200

    Added styles

---
+++ assets/js/main.js
@@ -0,0 +1,24 @@
+---
+---
+{% include_relative xp-data.js %}
+
+var buildChart = (context, data) => {
+  var options = {
+    responsive: true,
+    scaleShowLabels: false,
+    segmentStrokeWidth: 1,
+    tooltipTemplate: "<%= label %>"
+  }
+  new Chart(context).PolarArea(data, options)
+}
+
+var languageChart = $("#language-chart").get(0);
+var frameworkChart = $("#frameworks-chart").get(0);
+
+if (languageChart) {
+  buildChart(languageChart.getContext("2d"), languageData);
+}
+
+if (frameworkChart) {
+  buildChart(frameworkChart.getContext("2d"), frameworksData);
+}
---
+++ assets/js/xp-data.js
@@ -0,0 +1,77 @@
+var languageData = [
+  {
+      label: "Objective-C: 1 ano, 5 projetos",
+      value: 23,
+      color: "#435A75",
+      highlight: "#647282",
+  },
+  {
+      label: "JS: 4 anos, 5 projetos",
+      value: 19,
+      color: "#FDB45C",
+      highlight: "#FFCD8F",
+  },
+  {
+      label: "PHP: 2 anos, 2 projetos",
+      value: 18,
+      color: "#98A0CB",
+      highlight: "#CED0D9",
+  },
+  {
+      label: "Java: 3 meses, 1 projeto",
+      value: 10,
+      color: "#FF5300",
+      highlight: "#FF7733",
+  },
+  {
+      label: "Ruby: 3 anos, 7 projetos",
+      value: 30,
+      color:"#DC0012",
+      highlight: "#FC5751",
+  },
+]
+
+var frameworksData = [
+  {
+      label: "Rails: 3 anos, 5 projetos",
+      value: 26,
+      color:"#DC0012",
+      highlight: "#FC5751",
+  },
+  {
+      label: "iOS: 1 ano, 5 projetos",
+      value: 20,
+      color: "#599CFF",
+      highlight: "#8CBAFF",
+  },
+  {
+      label: "Sinatra: 5 meses, 2 projetos",
+      value: 17,
+      color: "#FFDCD9",
+      highlight: "#FFF3F2",
+  },
+  {
+      label: "AngularJS: 3 meses, 1 projeto",
+      value: 11,
+      color: "#DC0012",
+      highlight: "#FC5751",
+  },
+  {
+      label: "PHP Slim: 2 meses, 1 projeto",
+      value: 10,
+      color: "#000000",
+      highlight: "#0D0D0D",
+  },
+  {
+      label: "Laravel: 1 mês, 1 projeto",
+      value: 8,
+      color: "#E77037",
+      highlight: "#F5976C",
+  },
+  {
+      label: "Android: 3 meses, 1 projeto",
+      value: 8,
+      color: "#9CBF32",
+      highlight: "#B0CC5E",
+  }
+]
