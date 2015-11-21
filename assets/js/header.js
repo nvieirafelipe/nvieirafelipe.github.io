538f098cd4c492fabab5fe244097cd471152bd4a
Felipe Vieira <nvieirafelipe@gmail.com>
2015-11-21 13:46:05 -0200

    Fixes margins and remove unnecessary code

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,7 +1,6 @@
 var updateBackground = () => {
   var numberOfBg = 5;
   var imageNumber = Math.floor(Math.random() * (numberOfBg)) + 1;
-  var headerGradient = 'linear-gradient(rgba(0, 0, 0, 0.75), rgba(9, 113, 178, 0.25))';
   var headerImage = 'url("../assets/images/backgrounds/bg-0'+imageNumber+'.jpg")';
 
   $('header').css('background-image', headerImage);
