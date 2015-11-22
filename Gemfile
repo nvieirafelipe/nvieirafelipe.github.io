e19fce60192e8f3b2712a6444f3e84004257ae56
Felipe Vieira <nvieirafelipe@gmail.com>
2015-11-22 15:08:18 -0200

    Fixes code highlight syntax

--- Gemfile
+++ Gemfile
@@ -2,4 +2,5 @@ source 'https://rubygems.org'
 
 gem 'jekyll'
 gem 'jekyll-babel'
+gem 'kramdown'
 gem 'rouge'
--- assets/js/header.js
+++ assets/js/header.js
@@ -1,7 +1,7 @@
 var updateBackground = () => {
   var numberOfBg = 5;
   var imageNumber = Math.floor(Math.random() * (numberOfBg)) + 1;
-  var headerImage = 'url("../assets/images/backgrounds/bg-0'+imageNumber+'.jpg")';
+  var headerImage = 'url("/assets/images/backgrounds/bg-0'+imageNumber+'.jpg")';
 
   $('header').css('background-image', headerImage);
 }
