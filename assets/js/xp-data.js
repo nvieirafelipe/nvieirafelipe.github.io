25ab47a256e8031470d8d6a193f65dfd7b490617
Felipe Vieira <nvieirafelipe@gmail.com>
2015-11-22 16:23:05 -0200

    Update xp

--- assets/js/xp-data.js
+++ assets/js/xp-data.js
@@ -1,19 +1,25 @@
 var languageData = [
   {
+      label: "Ruby: 5 anos, 11 projetos",
+      value: 35,
+      color:"#DC0012",
+      highlight: "#FC5751",
+  },
+  {
       label: "Objective-C: 1 ano, 5 projetos",
-      value: 23,
+      value: 20,
       color: "#435A75",
       highlight: "#647282",
   },
   {
-      label: "JS: 4 anos, 5 projetos",
-      value: 19,
+      label: "JS: 5 anos, 9 projetos",
+      value: 20,
       color: "#FDB45C",
       highlight: "#FFCD8F",
   },
   {
-      label: "PHP: 2 anos, 2 projetos",
-      value: 18,
+      label: "PHP: 2 anos, 3 projetos",
+      value: 15,
       color: "#98A0CB",
       highlight: "#CED0D9",
   },
@@ -22,19 +28,13 @@ var languageData = [
       value: 10,
       color: "#FF5300",
       highlight: "#FF7733",
-  },
-  {
-      label: "Ruby: 3 anos, 7 projetos",
-      value: 30,
-      color:"#DC0012",
-      highlight: "#FC5751",
-  },
+  }
 ]
 
 var frameworksData = [
   {
-      label: "Rails: 3 anos, 5 projetos",
-      value: 26,
+      label: "Rails: 5 anos, 9 projetos",
+      value: 30,
       color:"#DC0012",
       highlight: "#FC5751",
   },
@@ -46,31 +46,37 @@ var frameworksData = [
   },
   {
       label: "Sinatra: 5 meses, 2 projetos",
-      value: 17,
+      value: 15,
       color: "#FFDCD9",
       highlight: "#FFF3F2",
   },
   {
+      label: "EmberJS: 7 meses, 1 projeto",
+      value: 14,
+      color: "#F08970",
+      highlight: "#E77558",
+  },
+  {
       label: "AngularJS: 3 meses, 1 projeto",
-      value: 11,
+      value: 10,
       color: "#DC0012",
       highlight: "#FC5751",
   },
   {
       label: "PHP Slim: 2 meses, 1 projeto",
-      value: 10,
+      value: 5,
       color: "#000000",
       highlight: "#0D0D0D",
   },
   {
       label: "Laravel: 1 mês, 1 projeto",
-      value: 8,
+      value: 3,
       color: "#E77037",
       highlight: "#F5976C",
   },
   {
       label: "Android: 3 meses, 1 projeto",
-      value: 8,
+      value: 3,
       color: "#9CBF32",
       highlight: "#B0CC5E",
   }
